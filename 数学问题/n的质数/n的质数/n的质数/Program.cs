f0f4ccf78b071c3022de0ad9cc85543a43284e3d
237041086 <237041086@qq.com>
2019-04-02 13:59:11 +0800

    n所有的质数

---
+++ 数学问题/n的质数/n的质数/n的质数/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace n的质数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountPrimes(100));
+        }
+        public static int CountPrimes(int n)
+        {
+
+            if (n <= 1)
+                return 0;
+
+            //用于存储是否为 质数的 标识
+            var temp = new bool[n];
+            var count = 0;
+            for (int i = 2; i < n; i++)
+            {
+                if (!temp[i])
+                {
+                    count++;
+                    //为质数，那么把它前面是此质数的倍数的位置标志为 非质数
+                    for (int j = 0; j * i < n; j++)
+                    {
+                        temp[j * i] = true;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}

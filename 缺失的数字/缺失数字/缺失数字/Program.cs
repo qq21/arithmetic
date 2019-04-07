d6e4d185715bc38aa93cf9471bc7adf3972602b1
zebin <237041086@qq.com>
2019-04-08 07:46:10 +0800

    缺失数字

---
+++ 缺失的数字/缺失数字/缺失数字/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 缺失数字
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int MissingNumber(int[] nums)
+        {
+
+
+            Array.Sort(nums);
+            if (nums.Length == 1 && nums[0] == 0) { return 1; }
+
+            if (nums.Length == 1) { return 0; }
+
+            if (nums.Length == 0) { return 0; }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i != nums[i])
+                {
+                    return i;
+
+                }
+            }
+            return nums.Length;
+        }
+    
+    }
+}
